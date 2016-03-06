932ce0505cfab14040195a39b3520682c943e893
Eugen <eugen@bopp.me>
2016-03-06 12:26:16 +0100

    Add filter additional exif filter

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -13,22 +13,34 @@ getHomeR = do
 
 data FilterOptions = FilterOptions
     { filterOptionsTag :: Text
+    , cameraManufacturer :: Maybe Text
+    , cameraModel :: Maybe Text
+    , flashFired :: Maybe Bool
     }
   deriving Show
 
 getTag :: Maybe FilterOptions -> Maybe Text
-getTag (Just (FilterOptions filterOptionsTag)) = Just filterOptionsTag
+getTag (Just (FilterOptions filterOptionsTag _ _ _)) = Just filterOptionsTag
 getTag Nothing = Nothing
 
 filterForm :: Maybe FilterOptions -> AForm Handler FilterOptions
 filterForm myFilterOptions = FilterOptions
     <$> areq textField (bfs ("Tag":: Text)) (getTag myFilterOptions)
+    <*> aopt textField (bfs ("Make"::Text)) Nothing
+    <*> aopt textField (bfs ("Camramodel"::Text)) Nothing
+    <*> aopt checkBoxField "Flash fired" Nothing
 
 postHomeR :: Handler Html
 postHomeR = do
     ((result, widget), enctype) <- runFormPost $ renderBootstrap3 BootstrapBasicForm (filterForm Nothing)
     case result of
          FormSuccess filterOptions' -> do
-             allFiles <- runDB $ selectList [PhotoTag ==. filterOptionsTag filterOptions'] [Desc PhotoTimeShot]
+             allFiles <- runDB $ selectList
+                (       [PhotoTag ==. filterOptionsTag filterOptions']
+                    ||. [PhotoCameraManufacturer ==. cameraManufacturer filterOptions']
+                    ||. [PhotoCameraModel ==. cameraModel filterOptions']
+                    ||. [PhotoFlashFired ==. flashFired filterOptions']
+                )
+                 [Desc PhotoTimeShot]
              defaultLayout $(widgetFile "homepage")
          _ -> sendResponseStatus status404 ("Not Found"::Text)
