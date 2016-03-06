519b63c64029997940046aaba2bf0d7aecbb7cc1
Eugen <eugen@bopp.me>
2016-03-06 11:48:12 +0100

    Add filter form to homepage

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -2,9 +2,11 @@ module Handler.Home where
 
 import Import
 import Text.Julius (RawJS (..))
+import Yesod.Form.Bootstrap3
 
 getHomeR :: Handler Html
 getHomeR = do
+    (widget, enctype) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm (filterForm Nothing)
     allFiles <- runDB $ selectList [] [Desc PhotoTime]
     defaultLayout $ do
         setTitle "Welcome To Yesod!"
