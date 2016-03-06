4b2d0d1c7e0f6bc9bd88c23360352bf16f7c2348
Eugen <eugen@bopp.me>
2016-03-06 18:24:33 +0100

    Add time photo created filter

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -2,6 +2,9 @@ module Handler.Home where
 
 import Import
 import Yesod.Form.Bootstrap3
+import Yesod.Form.Jquery
+import Data.Time.LocalTime
+import Data.Time.Clock as Clock
 
 getHomeR :: Handler Html
 getHomeR = do
@@ -16,11 +19,13 @@ data FilterOptions = FilterOptions
     , cameraManufacturer :: Maybe Text
     , cameraModel :: Maybe Text
     , flashFired :: Maybe Bool
+    , fromTimeShot :: Maybe Day
+    , toTimeShot :: Maybe Day
     }
   deriving Show
 
 getTag :: Maybe FilterOptions -> Maybe Text
-getTag (Just (FilterOptions filterOptionsTag _ _ _)) = Just filterOptionsTag
+getTag (Just (FilterOptions filterOptionsTag _ _ _ _ _)) = Just filterOptionsTag
 getTag Nothing = Nothing
 
 filterForm :: Maybe FilterOptions -> AForm Handler FilterOptions
@@ -29,18 +34,82 @@ filterForm myFilterOptions = FilterOptions
     <*> aopt textField (bfs ("Make"::Text)) Nothing
     <*> aopt textField (bfs ("Camramodel"::Text)) Nothing
     <*> aopt checkBoxField "Flash fired" Nothing
+    <*> aopt (jqueryDatePickerDayField def
+        { jdsChangeYear=True
+        , jdsYearRange="1950:−20"
+        }) "From date photo created" Nothing
+    <*> aopt (jqueryDatePickerDayField def
+        { jdsChangeYear=True
+        , jdsYearRange="1950:−20"
+        }) "To date photo created" Nothing
 
 postHomeR :: Handler Html
 postHomeR = do
     ((result, widget), enctype) <- runFormPost $ renderBootstrap3 BootstrapBasicForm (filterForm Nothing)
     case result of
          FormSuccess filterOptions' -> do
-             allFiles <- runDB $ selectList
-                (       [PhotoTag ==. filterOptionsTag filterOptions']
-                    ||. [PhotoCameraManufacturer ==. cameraManufacturer filterOptions']
-                    ||. [PhotoCameraModel ==. cameraModel filterOptions']
-                    ||. [PhotoFlashFired ==. flashFired filterOptions']
-                )
-                 [Desc PhotoTimeShot]
-             defaultLayout $(widgetFile "homepage")
+                let fromTime = dayToUtcTime $ fromTimeShot filterOptions'
+                    toTime = dayToUtcTime $ toTimeShot filterOptions'
+                    myFilter = map foo . filter bar $ createSqlFilters filterOptions'
+
+                allFiles <- runDB $ selectList myFilter [Desc PhotoTimeShot]
+                defaultLayout $(widgetFile "homepage")
          _ -> sendResponseStatus status404 ("Not Found"::Text)
+
+dayToUtcTime :: Maybe Day -> Maybe UTCTime
+dayToUtcTime (Just myDay) = Just $ UTCTime myDay $ Clock.secondsToDiffTime 0
+dayToUtcTime Nothing = Nothing
+
+-- foo :: FilterOptions -> [Filter Photo]
+-- foo filterOptions =
+--     (       [PhotoTag ==. filterOptionsTag filterOptions]
+--         ||. [PhotoCameraManufacturer ==. cameraManufacturer filterOptions]
+--         ||. [PhotoCameraModel ==. cameraModel filterOptions]
+--         ||. [PhotoFlashFired ==. flashFired filterOptions]
+--     )
+
+foo :: (Bool, a) -> a
+foo (_, a) = a
+
+bar :: (Bool, a) -> Bool
+bar (True, a) = True
+bar (False, a) = False
+
+checkIsBool :: Maybe a -> Bool
+checkIsBool (Just a) = True
+checkIsBool Nothing = False
+
+createSqlFilters :: FilterOptions -> [(Bool, Filter Photo)]
+createSqlFilters filterOptions =
+    [(True, Filter
+        { filterField=PhotoTag
+        , filterValue=Right [filterOptionsTag filterOptions]
+        , filterFilter=Eq
+        }
+     ),(isJust $ cameraManufacturer filterOptions, Filter
+        { filterField=PhotoCameraManufacturer
+        , filterValue=Right [cameraManufacturer filterOptions]
+        , filterFilter=Eq
+        }
+     ),(isJust $ cameraModel filterOptions, Filter
+        { filterField=PhotoCameraModel
+        , filterValue=Right [cameraModel filterOptions]
+        , filterFilter=Eq
+        }
+    ),(isJust $ flashFired filterOptions, Filter
+        { filterField=PhotoFlashFired
+        , filterValue=Right [flashFired filterOptions]
+        , filterFilter=Eq
+        }
+    ),(isJust $ fromTimeShot filterOptions, Filter
+        { filterField=PhotoTimeShot
+        , filterValue=Right [dayToUtcTime $ fromTimeShot filterOptions]
+        , filterFilter=Gt
+        }
+    ),(isJust $ toTimeShot filterOptions, Filter
+        { filterField=PhotoTimeShot
+        , filterValue=Right [dayToUtcTime $ toTimeShot filterOptions]
+        , filterFilter=Lt
+        }
+    )]
+
