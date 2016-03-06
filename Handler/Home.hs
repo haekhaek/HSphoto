31ff97b4d819daa8959c0810de25adc83bbf3feb
Eugen <eugen@bopp.me>
2016-03-06 11:52:56 +0100

    Add filter post route filter function to homepage

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -10,3 +10,25 @@ getHomeR = do
     defaultLayout $ do
         setTitle "Welcome To Yesod!"
         $(widgetFile "homepage")
+
+data FilterOptions = FilterOptions
+    { filterOptionsTag :: Text
+    }
+  deriving Show
+
+getTag :: Maybe FilterOptions -> Maybe Text
+getTag (Just (FilterOptions filterOptionsTag)) = Just filterOptionsTag
+getTag Nothing = Nothing
+
+filterForm :: Maybe FilterOptions -> AForm Handler FilterOptions
+filterForm myFilterOptions = FilterOptions
+    <$> areq textField (bfs ("Tag":: Text)) (getTag myFilterOptions)
+
+postHomeR :: Handler Html
+postHomeR = do
+    ((result, widget), enctype) <- runFormPost $ renderBootstrap3 BootstrapBasicForm (filterForm Nothing)
+    case result of
+         FormSuccess filterOptions' -> do
+             allFiles <- runDB $ selectList [PhotoTag ==. filterOptionsTag filterOptions'] [Desc PhotoTimeShot]
+             defaultLayout $(widgetFile "homepage")
+         _ -> sendResponseStatus status404 ("Not Found"::Text)
