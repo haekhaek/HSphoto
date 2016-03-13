f1da546bacccad78d11e0ed53d357cb6c5bf7451
Eugen <eugen@bopp.me>
2016-03-13 21:12:22 +0100

    Remove unused variable

--- Handler/Video.hs
+++ Handler/Video.hs
@@ -9,6 +9,6 @@ postVideoR = do
     case result of
         FormSuccess myForm -> do
             uploadedVideo <- formToFile myForm
-            videoId <- runDB $ insert uploadedVideo
+            _ <- runDB $ insert uploadedVideo
             sendResponseStatus status204 ("No Content"::Text)
         _ -> sendResponseStatus status400 ("Bad Request"::Text)
